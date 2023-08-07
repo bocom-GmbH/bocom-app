941c091ca9af911eb151ac217458b10a625b1972
baratoddavid <baratoddavid@gmail.com>
2023-08-07 11:54:59 +0200

    apollo added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,115 +2,60 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
-          Quasar App
+			Quasar App
         </q-toolbar-title>
-
+		<q-btn label="login" @click="loginFunc" />
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+		<router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
+import { defineComponent } from 'vue';
+import { useMutation } from '@vue/apollo-composable';
+import gql from 'graphql-tag'
 
 export default defineComponent({
-  name: 'MainLayout',
+	name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+	data () {
+		return {
+			username: 'test1',
+			password: 'test1'
+		}
+	},
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+	setup () {
+		let { mutate: login, error: loginError, loading: loading } = useMutation( 
+			gql`
+				mutation Login (
+					$username: String!,
+					$password: String!
+				) {
+					login (
+						username: $username,
+						password: $password
+					) {
+						token
+					}
+				}
+			`
+		)
 
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
+		return {
+			login
+		}
+	},
+	methods:{
+		async loginFunc(){
+			const loginData = await this.login({ username: this.username, password: this.password })
+		}
+	}
 });
 </script>
